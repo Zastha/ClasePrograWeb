3e21c8e5ba61e92f8b7362315f4df2068a07b25d
Zastha <my-email>
2025-10-28 12:50:01 -0700

    Clase Drag and Drop

---
+++ javascript_eventos/js/eventos.js
@@ -0,0 +1,43 @@
+let elemento = document.querySelector("#elemento");
+let agregar = document.querySelector("#agregar");
+let lista = document.querySelector("#listadinamica");
+elemento.focus();
+
+
+
+agregar.addEventListener("click",()=> {
+
+    if(elemento.value.trim() != ""){
+        let nuevoelemento=document.createElement("li");
+        nuevoelemento.innerText = elemento.value;
+        lista.appendChild(nuevoelemento);
+        elemento.value="";
+        
+    }
+});
+
+
+elemento.addEventListener("keydown",(e)=>{
+    if(e.key == "Enter"){
+        agregar.click();
+    }
+})
+
+//Arrastrar y soltar
+let arrastra = document.querySelector("#arrastra");
+let zonasoltar = document.getElementById("zonasoltar");
+
+arrastra.addEventListener("dragstart",function(e){
+    this.style.border = "5px solid red";
+} )
+
+zonasoltar.addEventListener("dragover", function(e){
+    //zonasoltar olvida que no se pueden
+    //arastrar elementos sobre el
+    event.preventDefault();
+})
+
+zonasoltar.addEventListener("drop", function(e){
+    arrastra.style.border="none";
+    this.append(arrastra);
+})
