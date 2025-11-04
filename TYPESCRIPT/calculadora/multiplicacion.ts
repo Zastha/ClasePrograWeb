c4672ac5500066e2bb00d79c1d6984a737aad43a
Zastha <my-email>
2025-11-04 12:51:00 -0700

    calculadora typescript

--- TYPESCRIPT/calculadora/multiplicacion.ts
+++ TYPESCRIPT/calculadora/multiplicacion.ts
@@ -1,5 +1,43 @@
-const multiplicar = (a,b,imprimir)=>{
+const multiplicar = (a:number,b:number,imprimir:string)=>{
     console.log(imprimir, a*b);
 }
 
+
+const operaciones = ['suma','resta','division','multiplicacion','+','-','*','/']
+
+type Operaciones = 'suma'|'resta'|'division'|'multiplicacion' | '+' | '-' | '/' | '*'
+
+//type Resultados = number|string
+
+const calculadora= (a:number,b:number,operacion: Operaciones):number =>{
+
+    if(operacion == 'suma' || operacion == '+'){
+        return a+b;
+    }
+    if(operacion == 'resta' || operacion == '-'){
+        return a-b;
+    }
+    if(operacion == 'multiplicacion' || operacion == '*'){
+        return a*b;
+    }
+    if((operacion == 'division' || operacion == '/')){
+
+        if(b == 0){
+            throw new Error("No se puede dividir entre cero");
+        }
+
+        return a/b;
+    }
+
+    throw new Error("Operacion no Valida");
+}
+
 multiplicar(2,4,"El resultado es: ")
+
+
+console.log(calculadora(5,6,"suma"))
+console.log(calculadora(5,6,"resta"))
+console.log(calculadora(5,6,"multiplicacion"))
+console.log(calculadora(5,6,"division"))
+console.log(calculadora(5,0,"division"))
+//console.log(calculadora(5,6,"sumaste"))
