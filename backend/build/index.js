feca1ef3118ee736ff226d37acd68904785c6504
Zastha <my-email>
2025-11-11 12:52:22 -0700

    Request Response API

--- backend/build/index.js
+++ backend/build/index.js
@@ -1,2 +1,20 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+import express from 'express';
+express;
+//Creamos la aplicacion a traves del paquete expresss
+//y llamamos a su constructor
+const app = express();
+//Todo lo que regresa al usuario es tipo JSON
+app.use(express.json());
+//Para escuchar las peticiones del frontend
+const PUERTO = 3001;
+//RUTAS
+app.get('/', (_request, response) => {
+    response.send('Estoy en la raiz del servidor');
+});
+app.get('/HOLA', (_request, response) => {
+    response.send('Estoy en la HOLA del servidor');
+});
+//Encendemos el servidor y lo ponemos a escuchar peticiones
+app.listen(PUERTO, () => {
+    console.log(`Servidor Encendido escuchando en el puerto ${PUERTO}`);
+});
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,31 @@
 import express from 'express';
+express
+
+//Creamos la aplicacion a traves del paquete expresss
+//y llamamos a su constructor
+const app = express();
+
+//Todo lo que regresa al usuario es tipo JSON
+app.use(express.json());
+
+//Para escuchar las peticiones del frontend
+const PUERTO:number = 3001;
+
+//RUTAS
+app.get('/',(_request,response)=>{
+
+    response.send('Estoy en la raiz del servidor')
+
+})
+
+app.get('/HOLA',(_request,response)=>{
+
+    response.send('Estoy en la HOLA del servidor')
+
+})
+
+//Encendemos el servidor y lo ponemos a escuchar peticiones
+app.listen(PUERTO, ()=>{
+    console.log(`Servidor Encendido escuchando en el puerto ${PUERTO}`);
+});
+
